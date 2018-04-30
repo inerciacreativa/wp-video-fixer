e0476f56f1846aca258343fb15d038789f8d7c7c
Jose Cuesta <jose.cuesta@inerciacreativa.com>
2018-04-30 19:06:03 +0200

    Updated version

--- video-fixer.php
+++ video-fixer.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: ic Video Fixer
  * Plugin URI:  https://github.com/inerciacreativa/wp-video-fixer
- * Version:     1.0.0
+ * Version:     2.0.0
  * Text Domain: ic-video-fixer
  * Domain Path: /languages
  * Description: Soluciona problemas al insertar vídeos de EiTB, DIPC y IAA.
