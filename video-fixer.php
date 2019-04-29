d5363ccfe192a888ca954e1b76b902c876f69034
Jose Cuesta <jose.cuesta@inerciacreativa.com>
2019-04-29 20:43:54 +0200

    Updated version

--- video-fixer.php
+++ video-fixer.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: ic Video Fixer
  * Plugin URI:  https://github.com/inerciacreativa/wp-video-fixer
- * Version:     2.1.0
+ * Version:     4.0.0
  * Text Domain: ic-video-fixer
  * Domain Path: /languages
  * Description: Soluciona problemas al insertar vídeos de EiTB, DIPC y IAA.
@@ -12,8 +12,26 @@
  * License URI: https://opensource.org/licenses/MIT
  */
 
+use ic\Framework\Framework;
+use ic\Plugin\VideoFixer\VideoFixer;
+
 if (!defined('ABSPATH')) {
 	exit;
 }
 
-ic\Plugin\VideoFixer\VideoFixer::create(__FILE__);
+if (!class_exists(Framework::class)) {
+	throw new RuntimeException(sprintf('Could not find %s class.', Framework::class));
+}
+
+if (!class_exists(VideoFixer::class)) {
+	$autoload = __DIR__ . '/vendor/autoload.php';
+
+	if (file_exists($autoload)) {
+		/** @noinspection PhpIncludeInspection */
+		include_once $autoload;
+	} else {
+		throw new RuntimeException(sprintf('Could not load %s class.', VideoFixer::class));
+	}
+}
+
+VideoFixer::create(__FILE__);
