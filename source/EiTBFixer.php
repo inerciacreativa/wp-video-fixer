15821a3c08ee212ae327946c4dc6de3ce69a1de6
Jose Cuesta <jose.cuesta@inerciacreativa.com>
2020-02-05 19:17:12 +0100

    Bugfix: check if data could be retrieved from API

--- source/EiTBFixer.php
+++ source/EiTBFixer.php
@@ -39,22 +39,24 @@ class EiTBFixer extends AbstractFixer implements FixerInterface
 				continue;
 			}
 
-			$info  = $this->getUrlInfo($url);
-			$embed = null;
+			$id   = $this->getIdFromUrl($url);
+			$info = null;
 
-			if (count($info) === 2) {
-				$embed = $this->getEmbedInfo($info);
+			if (count($id) === 2) {
+				$info = $this->getInfoFromApi($id);
 			}
 
-			if ($embed) {
-				if ($info['type'] === 'video') {
-					$video  = $this->getVideo($dom, $embed->FILE_MP4, $this->addScheme($embed->IMAGENEMBED));
-					$figure = $this->getFigure($dom, $video, $embed->TITULO, $embed->URL);
-				} else {
-					$audio  = $this->getAudio($dom, $embed->URL_AUDIO);
+			if ($info) {
+				if ($id['type'] === 'video' && isset($info->FILE_MP4, $info->IMAGENEMBED, $info->TITULO, $info->URL)) {
+					$video  = $this->getVideo($dom, $info->FILE_MP4, $this->addScheme($info->IMAGENEMBED));
+					$figure = $this->getFigure($dom, $video, $info->TITULO, $info->URL);
+				} else if (isset($info->URL_AUDIO)) {
+					$audio  = $this->getAudio($dom, $info->URL_AUDIO);
 					$figure = $this->getFigure($dom, $audio);
 				}
+			}
 
+			if (isset($figure)) {
 				$element->parentNode->parentNode->replaceChild($figure, $element->parentNode);
 			} else {
 				$url = str_replace($this->url('es/get/multimedia/screen/'), $this->url('es/get/multimedia/embed/'), $url);
@@ -86,7 +88,7 @@ class EiTBFixer extends AbstractFixer implements FixerInterface
 	 *
 	 * @return array
 	 */
-	protected function getUrlInfo(string $url): array
+	protected function getIdFromUrl(string $url): array
 	{
 		$path = explode('/', parse_url($url, PHP_URL_PATH));
 		$info = [];
@@ -109,18 +111,13 @@ class EiTBFixer extends AbstractFixer implements FixerInterface
 	}
 
 	/**
-	 * @param array $info
+	 * @param array $id
 	 *
 	 * @return null|object
 	 */
-	protected function getEmbedInfo(array $info): ?object
+	protected function getInfoFromApi(array $id): ?object
 	{
-		$query = self::QUERY . self::$parameters[$info['type']];
-		$query = str_replace(['%ID%', '%TYPE%'], [
-			$info['id'],
-			$info['type'],
-		], $query);
-
+		$query  = $this->getApiQuery($id);
 		$result = get_transient($query);
 
 		if ($result === false) {
@@ -145,4 +142,19 @@ class EiTBFixer extends AbstractFixer implements FixerInterface
 		return $result === 'null' ? null : $result;
 	}
 
+	/**
+	 * @param array $id
+	 *
+	 * @return string
+	 */
+	protected function getApiQuery(array $id): string
+	{
+		$query = self::QUERY . self::$parameters[$id['type']];
+		$query = str_replace(['%ID%', '%TYPE%'], [
+			$id['id'],
+			$id['type'],
+		], $query);
+
+		return $query;
+	}
 }
